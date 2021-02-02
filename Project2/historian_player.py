0857279b12b6049595fafbcd022a1bb7b78d35f0
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-02 18:39:04 +0100

    add stuff to historian

--- Project2/historian_player.py
+++ Project2/historian_player.py
@@ -3,6 +3,7 @@ test
 """
 
 
+import random
 from player import Player
 from action import Action
 
@@ -12,14 +13,30 @@ class HistorianPlayer(Player):
         super().__init__(name)
         self.history = []
         self.remember = remember
+        self.is_beaten_by = {
+            "rock": "paper",
+            "paper": "scissor",
+            "scissor": "rock"
+        }
 
     def select_action(self):
+        most_common_moves = {
+          "rock": 0,
+          "paper": 0,
+          "scissor": 0
+        }
         n_last_sequence = self.history[-self.remember:]
+
+        # Returns a random value if the history is empty
+        if len(n_last_sequence) == 0:
+            random_value = random.randint(0, 2)
+            value = self.action_map[random_value]
+            return Action(value)
+
         for i in range(len(self.history)):
             temp_list = self.history[i:i + len(n_last_sequence)]
             if temp_list == n_last_sequence:
-                #Add self.history[i + len(n_last_sequence) + 1]
-
+                return
 
     def receive_result(self, action):
         self.history.append[action.value]
