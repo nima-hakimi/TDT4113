ff174c282a98b314fe7f425012f67ef35571787b
unknown <nimah@stud.ntnu.no>
2021-02-02 17:53:26 +0100

    Created class historian_player

---
+++ Project2/historian_player.py
@@ -0,0 +1,18 @@
+"""
+test
+"""
+
+
+from player import Player
+from action import Action
+
+
+class HistorianPlayer(Player):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def select_action(self):
+
+
+    def receive_result(self, action):
+        pass
