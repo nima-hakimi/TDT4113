a8f7b4ae2e07273b923cd97b420bd64255f2ecdd
unknown <nimah@stud.ntnu.no>
2021-02-02 17:43:34 +0100

    Completed most_common_player class

--- Project2/most_common_player.py
+++ Project2/most_common_player.py
@@ -17,8 +17,15 @@ class MostCommonPlayer(Player):
         }
 
     def select_action(self):
-        value = "rock"
-        return Action(value)
+        most_common_move = ""
+        number_of_common_moves = -1
+        for key in self.previous_moves.keys():
+            if self.previous_moves[key] > number_of_common_moves:
+                number_of_common_moves = self.previous_moves[key]
+                most_common_move = key
+        most_common_action = Action(most_common_move)
+        beats_most_common_action = Action(most_common_action.beats[most_common_move])
+
 
     def receive_result(self, action):
-        pass
+        self.previous_moves[action.value] += 1
