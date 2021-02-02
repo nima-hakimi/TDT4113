1c8a33e3bb95d16c91afab6b45acdedefe28bcd5
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-02 16:34:57 +0100

    add sequential player

---
+++ Project2/sequential_player.py
@@ -0,0 +1,23 @@
+"""
+test
+"""
+
+
+import random
+from player import Player
+from action import Action
+
+
+class SequentialPlayer(Player):
+    def __init__(self, name):
+        super().__init__(name)
+        self.counter = 0
+
+    def select_action(self):
+        counter_mod_3 = counter % 3
+        value = self.action_map[counter_mod_3]
+        counter += 1
+        return Action(value)
+
+    def receive_result(self, action):
+        pass
