9febe8e74bfe1b5f765e458b2ac7931778feaba6
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-02 18:00:53 +0100

    small fix in most common player

--- Project2/most_common_player.py
+++ Project2/most_common_player.py
@@ -15,6 +15,11 @@ class MostCommonPlayer(Player):
           "paper": 0,
           "scissor": 0
         }
+        self.is_beaten_by = {
+            "rock": "paper",
+            "paper": "scissor",
+            "scissor": "rock"
+        }
 
     def select_action(self):
         most_common_move = ""
@@ -23,9 +28,9 @@ class MostCommonPlayer(Player):
             if self.previous_moves[key] > number_of_common_moves:
                 number_of_common_moves = self.previous_moves[key]
                 most_common_move = key
-        most_common_action = Action(most_common_move)
-        beats_most_common_action = Action(most_common_action.beats[most_common_move])
-        return beats_most_common_action
+        print(self.previous_moves)
+        print(most_common_move)
+        return Action(self.is_beaten_by[most_common_move])
 
     def receive_result(self, action):
         self.previous_moves[action.value] += 1
