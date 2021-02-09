ea8dc0073a7306832790f799ff0887ece64870bb
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-09 23:17:50 +0100

    sender pylint

--- Project3/persons/sender.py
+++ Project3/persons/sender.py
@@ -3,17 +3,20 @@ A person which will send (encrypt) encrypted messages
 """
 
 
-from .person import Person
+from person import Person
 
 class Sender(Person):
-  def __init__(self, cipher):
-    super().__init__(cipher)
+    """ The Person to decrypt and send the message to a receiver """
+    def __init__(self, cipher):
+        super().__init__(cipher)
 
-  def operate_cipher(self, message):
-    return self.cipher.encode(message, self._key)
+    def operate_cipher(self, message):
+        """ Perform encode """
+        return self.cipher.encode(message, self._key)
 
-  def send(self, message, receiver):
-    self.message = message
-    encoded_message = self.operate_cipher(message)
-    self.encoded_message = encoded_message
-    receiver.receive(encoded_message)
+    def send(self, message, receiver):
+        """ Encode the message and send it to the receiver """
+        self.message = message
+        encoded_message = self.operate_cipher(message)
+        self.encoded_message = encoded_message
+        receiver.receive(encoded_message)
