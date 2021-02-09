a7c605a10973db7bfed22fe141788f978ff7fa12
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-09 23:01:33 +0100

    person lint

--- Project3/persons/person.py
+++ Project3/persons/person.py
@@ -4,14 +4,17 @@ Superclass for persons
 
 
 class Person:
-  def __init__(self, cipher):
-    self.cipher = cipher
-    self._key = None
-    self.message = ''
-    self.encoded_message = ''
-  
-  def set_key(self, key):
-    self._key = key
+    """ Superclass for Sender and Receiver """
+    def __init__(self, cipher):
+        self.cipher = cipher
+        self._key = None
+        self.message = ''
+        self.encoded_message = ''
 
-  def get_key(self):
-    return self._key
+    """ Updates key """
+    def set_key(self, key):
+        self._key = key
+
+    """ Retrieves key """
+    def get_key(self):
+        return self._key
