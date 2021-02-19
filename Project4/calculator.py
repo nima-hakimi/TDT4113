258e5a65f3ebafae87bb7a4da11aec8ba4a9a582
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-19 14:14:46 +0100

    Define method names - skeleton not ready

--- Project4/calculator.py
+++ Project4/calculator.py
@@ -16,8 +16,44 @@ class Calculator:
                           'SQRT': Function(numpy.sqrt)}
 
         self.operators = {'ADD': Operator(numpy.add, 0),
+                          'PLUS': Operator(numpy.add, 0),
                           'MULTIPLY': Operator(numpy.multiply, 1),
                           'DIVIDE': Operator(numpy.divide, 1),
-                          'SUBTRACT': Operator(numpy.subtract, 0)}
+                          'SUBTRACT': Operator(numpy.subtract, 0),
+                          'MINUS': Operator(numpy.subtract, 0)}
 
         self.output_queue = Queue()
+
+    def evaluate(self):
+        """
+        ...
+        """
+        pass
+
+    def convert_queue_to_rpn(self):
+        """
+        ...
+        """
+        pass
+
+    def text_parser(self):
+        """
+        ...
+        """
+        pass
+
+    def calculate(self):
+        """
+        Put everything together
+        """
+        pass
+
+
+def test():
+    calc = Calculator ()
+    print(calc.functions['EXP'].execute(
+          calc.operators['ADD'].execute (1,
+          calc.operators['MULTIPLY'].execute (2, 3))))
+
+if __name__ == '__main__':
+    test()
