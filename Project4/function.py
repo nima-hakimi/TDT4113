fc1993ac03c0e33aaba7167fb072bc1b39bc277f
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-02-20 12:01:42 +0100

    remove stuff

--- Project4/function.py
+++ Project4/function.py
@@ -11,7 +11,7 @@ class Function:
     def __init__(self, func):
         self.func = func
 
-    def execute(self, element, debug=True):
+    def execute(self, element, debug=False:
         if not isinstance(element, numbers.Number):
             raise TypeError('The element must be a number')
         result = self.func(element)
